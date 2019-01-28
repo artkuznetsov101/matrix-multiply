f1fa722d65fa4ea4728fc32e5f48261227f763b9
Artem Kuznetsov <artkuznetsov101@gmail.com>
2019-01-28 15:08:33 +0300

    fix

--- src/test/java/com/test/SimpleLoopTest.java
+++ src/test/java/com/test/SimpleLoopTest.java
@@ -14,7 +14,7 @@ public class SimpleLoopTest {
 	public void setUpBefore() throws Exception {
 		multiplier = MultiplierFactory.getSimpleLoopMultiplier();	
 		m1 = MatrixHelper.createMatrix1(Multiplier.size);
-		m2 = MatrixHelper.createMatrix1(Multiplier.size);	
+		m2 = MatrixHelper.createMatrix2(Multiplier.size);	
 	}
 	
 	@Test
